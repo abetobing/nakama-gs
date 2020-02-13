570ab1ada90f2e6436f5d6f8f7cbf4980fe2105a
abetobing <abe@elevenia.co.id>
2020-02-13 18:33:12 +0700

    change to context.match_id as key

--- modules/match_joined_create_spawn.lua
+++ modules/match_joined_create_spawn.lua
@@ -1,19 +1,19 @@
 local nk = require("nakama")
 
-local function match_joined_create_spawn(context, outgoing_payload,
-                                         incoming_payload)
-
+local function match_joined_create_spawn(context, outgoing_payload, incoming_payload)
     local possible_spawn_pos = {
-        {p = {x = -15, y = 0, z = 17}},
-        {p = {x = 13, y = 0, z = 17}}
+        {pos = {x = -15, y = 0, z = 17}},
+        {pos = {x = 13, y = 0, z = 17}}
     }
 
     local objectid = {
         collection = "spawns",
-        key = outgoing_payload.match.match_id
+        key = context.match_id
     }
     local spawns = nk.storage_read(objectid)
     local existing_spawns = spawns.value
+    nk.logger_info(("Context: %q"):format(context))
+    nk.logger_info(("Existing spawns: %q"):format(existing_spawns))
 
     -- math.randomseed(os.time())
     local value = {}
@@ -25,18 +25,17 @@ local function match_joined_create_spawn(context, outgoing_payload,
         value = possible_spawn_pos[0]
         existing_spawns[0] = value
     end
+    nk.logger_info(("Value: %q"):format(value))
 
     local object = {
         collection = "spawns",
         -- key = context.user_id,
         -- user_id = context.user_id,
-        key = outgoing_payload.match.match_id,
+        key = context.match_id,
         value = existing_spawns
     }
     nk.storage_write({object})
-    nk.logger_info(("Payload HOOK [match_joined_create_spawn]: %q"):format(
-                       outgoing_payload))
-
+    nk.logger_info(("Payload HOOK [match_joined_create_spawn]: %q"):format(outgoing_payload))
 end
 
 nk.register_rt_after(match_joined_create_spawn, "MatchJoin")
