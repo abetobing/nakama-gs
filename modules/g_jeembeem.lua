714d7766dda47110fcb08c17a7cb158cf9796991
abetobing <abe@elevenia.co.id>
2020-03-09 20:08:27 +0700

    handles receiving attack damage event

--- modules/g_jeembeem.lua
+++ modules/g_jeembeem.lua
@@ -27,7 +27,7 @@ function M.match_leave(context, dispatcher, tick, state, presences)
   for _, presence in ipairs(presences) do
     state.presences[presence.session_id] = nil
     nk.logger_info(("---- someone leave the match: %q -------"):format(presence.user_id))
-end
+  end
   return state
 end
 
@@ -35,18 +35,20 @@ function M.match_loop(context, dispatcher, tick, state, messages)
   for _, msg in ipairs(messages) do
     -- nk.logger_info(("Received MSG: %s"):format(nk.json_encode(msg)))
     -- nk.logger_info(("Received %s from %s"):format(msg.data, msg.sender.username))
-    local decoded = nk.json_decode(msg.data)
+    -- local decoded = nk.json_decode(msg.data)
 
     -- Broadcast message back to all
     -- local presences = nil
-    if msg ~= nil
-    then
-        -- Process based on op_code
-        if msg.op_code == 201
-        then
-          -- nk.logger_info(("Broadcasting message MSG: %s"):format(nk.json_encode(msg)))
-          dispatcher.broadcast_message(msg.op_code, msg.data, nil, msg.sender)
-        end
+    if msg ~= nil then
+      -- Process based on op_code
+      if msg.op_code == 201 then
+        -- nk.logger_info(("Broadcasting message MSG: %s"):format(nk.json_encode(msg)))
+        dispatcher.broadcast_message(msg.op_code, msg.data, nil, msg.sender)
+      elseif msg.op_code == 301 then
+        dispatcher.broadcast_message(msg.op_code, msg.data, nil, msg.sender)
+      elseif (msg.op_code == 401) then
+        dispatcher.broadcast_message(msg.op_code, msg.data, nil, msg.sender);
+      end
     end
   end
   return state
